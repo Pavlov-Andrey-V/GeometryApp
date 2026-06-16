274e2df7dfaef3e9cea6cf0be788535566b6da25
apavlov <pavlov.and.v@mail.ru>
2026-06-16 12:20:29 +0300

    First commit: created GeometryApp with GeometryLibrary dependecy

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import com.geometry.shapes.Circle;
+import com.geometry.shapes.Rectangle;
+import com.geometry.shapes.Triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+        Triangle triangle = new Triangle(3, 5, 6);
+
+        System.out.printf("Круг: площадь = %.2f, периметр = %.2f%n", circle.getArea(), circle.getPerimeter());
+        System.out.printf("Прямоугольник: площадь = %.2f, периметр = %.2f%n", rectangle.getArea(), rectangle.getPerimeter());
+        System.out.printf("Треугольник: площадь = %.2f, периметр = %.2f%n", triangle.getArea(), triangle.getPerimeter());
+    }
+}
